8de9def40c82c93994c92ba3d0bf0f78062a9ccb
Charansaimsc <charanmsc7969@gmail.com>
2026-01-21 11:48:09 +0530

    [charan]Add. Testing code using several test cases on same method

---
+++ Nunit/parameterizedTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class NumberUtilsTests
+{
+    private NumberUtils utils;
+
+    [SetUp]
+    public void Setup()
+    {
+        utils = new NumberUtils();
+    }
+
+    [TestCase(2, true)]
+    [TestCase(4, true)]
+    [TestCase(6, true)]
+    [TestCase(7, false)]
+    [TestCase(9, false)]
+    public void IsEven_TestCases(int input, bool expected)
+    {
+        bool result = utils.IsEven(input);
+
+        Assert.AreEqual(expected, result);
+    }
+}
