36f108c1d18fbae2c3c52a2af8c1411bec62ad70
Charansaimsc <charanmsc7969@gmail.com>
2026-01-21 11:50:01 +0530

    [charan]Add. Code to understand about attribute setup and teardown

---
+++ Nunit/SetUpAndTearDown/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace DatabaseTests
+{
+    [TestFixture]
+    public class DatabaseConnectionTests
+    {
+        private DatabaseConnection db;
+
+        // Runs BEFORE each test
+        [SetUp]
+        public void Setup()
+        {
+            db = new DatabaseConnection();
+            db.Connect();
+        }
+
+        // Runs AFTER each test
+        [TearDown]
+        public void TearDown()
+        {
+            db.Disconnect();
+        }
+
+        [Test]
+        public void Connect_EstablishesConnection()
+        {
+            Assert.IsTrue(db.IsConnected);
+        }
+
+        [Test]
+        public void Disconnect_ClosesConnection()
+        {
+            db.Disconnect();
+            Assert.IsFalse(db.IsConnected);
+        }
+    }
+}
