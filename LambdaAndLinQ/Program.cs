0840af66be443715d2d021813f7bb463acf51bf7
Charansaimsc <charanmsc7969@gmail.com>
2026-02-09 14:37:50 +0530

    [charan]Add. Main Method for all Lambda and LinQ problems

---
+++ LambdaAndLinQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lambdaa1;
+
+namespace Lambdaa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EmployeeService.Filter();
+            //StringSorter.Run();
+             ProductService.GroupAverage();
+        }
+    }
+}
